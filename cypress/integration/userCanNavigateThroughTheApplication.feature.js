a2e8e4112bd45a15558db929c9fd9aa62cb040c5
johnnysee <js.johnny.see@gmail.com>
2021-09-07 00:09:17 +0200

    rewrote the testing spec where user can navigate through application

--- cypress/integration/userCanNavigateThroughTheApplication.feature.js
+++ cypress/integration/userCanNavigateThroughTheApplication.feature.js
@@ -3,38 +3,72 @@ describe("User can navigate the app", () => {
     cy.visit("http://localhost:3000")
   });
 
-  describe("to About tap and it", () => {
+  describe("to About tab and it", () => {
     beforeEach(() => {
-      cy.get("#about-tap")
-    })
-  })
+      cy.get("#about-tap").click();
+    });
 
-  it("to About tab", () => {
-    cy.get("#about-tab").click();
+    it("displays About Me header", () => {
+      cy.get("#about-header").should("contain", "About Me");
+    });
 
-    cy.get("#about-header").should("contain", "About Me");
+    it("displays component name in url", () => {
+      cy.url().should("contain", "about");
+    });
 
-    cy.get("#projects-header").should("not.exist");
-    cy.get("#hello").should("not.exist");
+    it("does not display My Projects header ", () => {
+      cy.get("projects-header").should("not.exist");
+    });
+
+    it("does not display Hello world", () => {
+      cy.get("#hello").should("not.exist");
+    });
   });
 
-  it("to My Projects tab", () => {
-    cy.get("#projects-tab").click();
+  describe("to My Projects tab and it", () => {
+    beforeEach(() => {
+      cy.get("projects-tab").click();
+    });
 
-    cy.get("#projects-header").should("contain", "My Projects");
+    it("displays My Projects header", () => {
+      cy.get("#projects-header").should("contain", "My Projects");
+    });
 
-    cy.get("#about-header").should("not.exist");
-    cy.get("#hello").should("not.exist");
+    it("displays component name in url", () => {
+      cy.url().should("contain", "projects");
+    });
 
-  })
+    it("does not display About Me header ", () => {
+      cy.get("#about-header").should("not.exist");
+    });
 
-  it("back to My Portfolio/Hello tab", () => {
-    cy.get("#about-tab").click();
-    cy.get("#header").click();
+    it("does not display Hello world", () => {
+      cy.get("#hello").should("not.exist");
+    });
+  });
 
-    cy.get("#hello").should("contain", "Hello");
+  describe("back to My Portfolio/Hello tab and it", () => {
+    beforeEach(() => {
+      cy.get("#about-tab").click();
+      cy.get("#header").click();
+    });
 
-    cy.get("#projects-header").should("not.exist");
-    cy.get("#about-header").should("not.exist");
-  })
-})
+    it("displays Hello World", () => {
+      cy.get("#hello").should("contain", "Hello")
+    });
+
+    it("displays correct url", () => {
+      cy.url()
+      .should("not.contain", "projects")
+      .and("not.contain", "about");
+    });
+
+    it("does not display About Me header ", () => {
+      cy.get("#about-header").should("not.exist");
+    });
+
+    it("does not display My Projects header", () => {
+      cy.get("#projects-header").should("not.exist");
+    });
+  });
+});
