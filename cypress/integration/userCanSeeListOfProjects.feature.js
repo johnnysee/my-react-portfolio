3013f6c7d369dc1e3015e86ab516109c656fcf94
johnnysee <js.johnny.see@gmail.com>
2021-09-07 16:22:51 +0200

    removed date at footer added personal contant and adjusted accordingly in the spec file with projects.json file

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -7,18 +7,18 @@ describe("User can see list of projects", () => {
   it("displays the first project", () => {
     cy.get("#project-1").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("contain", "My First Website");
-      cy.get(".description").should("contain", "This was my first project.");
+      cy.get(".header").should("contain", "ATM Machine App");
+      cy.get(".description").should("contain", "This was my first ruby application. It is an ATM machine where the user can create a person, account and an ATM in terminal.");
     });
   });
 
   it("displays the second project", () => {
     cy.get("#project-2").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("contain", "FizzBuzz");
+      cy.get(".header").should("contain", "Library App");
       cy.get(".description").should(
         "contain",
-        "Making ruby backend application is fun."
+        "It is a library application written in Ruby. A user can create a library and a visitor in terminal"
       );
     });
   });
@@ -26,10 +26,10 @@ describe("User can see list of projects", () => {
   it("displays the third object", () => {
     cy.get("#project-3").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("contain", "Mobile UX");
+      cy.get(".header").should("contain", "Address Book App");
       cy.get(".description").should(
         "contain",
-        "I like to design for the mobile platform."
+        "It is an Address Book App where the user can add new contacts which will be added in a list."
       );
     });
   });
