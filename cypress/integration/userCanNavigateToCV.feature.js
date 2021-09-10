13858a53001fb12846a9c860dff04677f2d404dd
johnnysee <js.johnny.see@gmail.com>
2021-09-10 17:39:09 +0200

    added userCanNavigateToCV spec file there are 1 visit and 4 it blocks

---
+++ cypress/integration/userCanNavigateToCV.feature.js
@@ -0,0 +1,21 @@
+describe("User can navigate to CV", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+    
+  it("is expected to be able to click on CV", () => {
+    cy.get("#cv-tab").click();
+  })
+
+  it("is expected to contain cv in header" , () => {
+    cy.get("#header").should("contain", "CV");
+  })
+
+  it("is expected to contain Work Experience", () => {
+    cy.get('#cv').should("contain", "Work Experience")
+  })
+
+  it("is expected to contain Education", () => {
+    cy.get('#cv').should("contain", "Education")
+  })
+})
