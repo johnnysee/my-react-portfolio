d495952e0ba2454ad691064165cd078226e5fe17
johnnysee <js.johnny.see@gmail.com>
2021-09-12 20:09:42 +0200

    split the summaries into separated summaries in work.json

--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -9,6 +9,17 @@ const CvCard = ({ work }) => {
      <List.Item>{work.startDate}</List.Item>
      <List.Item>{work.endDate}</List.Item>
      <List.Item>{work.summary}</List.Item>
+     <List.Item>{work.summary2}</List.Item>
+     <List.Item>{work.summary3}</List.Item>
+     <List.Item>{work.summary4}</List.Item>
+     <List.Item>{work.summary5}</List.Item>
+     <List.Item>{work.summary6}</List.Item>
+     <List.Item>{work.summary7}</List.Item>
+     <List.Item>{work.summary8}</List.Item>
+     <List.Item>{work.summary9}</List.Item>
+     <List.Item>{work.summary10}</List.Item>
+     <List.Item>{work.summary11}</List.Item>
+     <List.Item>{work.summary12}</List.Item>
     </List>
   )
 }
