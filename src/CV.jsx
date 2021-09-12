2b63d9ccd13c8ffabce75a2620e6c65cdc8e4d0c
johnnysee <js.johnny.see@gmail.com>
2021-09-12 20:11:26 +0200

    removed hardcoded cv info

--- src/CV.jsx
+++ src/CV.jsx
@@ -38,26 +38,3 @@ class Cv extends Component {
 
 export default Cv;
 
-
-
-// Hardcoded below.
-
-      {/* <h1 id="cv-tab">CV</h1>
-      <h2 id="cv-tab">Work Experience</h2>
-      <h3>Wiring & Harness Engineer</h3>
-      <h4>Volvo Buses</h4>
-      <ul>
-        <li>
-        Design electrical schematics and bundle drawings for suppliers, customer adaptation, aftermarket and on-going production release.
-        </li>
-        <li>
-        Tools: SaberES Designer 2020 and Saber Harness 2015
-        </li>
-        <li>
-        PDM-system KOLA
-        </li>
-      </ul>
-      <h2 id="cv-tab">Education</h2>
-      <h3>Linköping University</h3>
-      <h4>Bachelor's degree, Mechanical Engineering, Bachelor of Science</h4>
-      <p>2014 - 2017</p> */}
